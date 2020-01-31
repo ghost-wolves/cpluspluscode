b723e212492b67c902e8ac5d1e700b432ba3def6
Sawyer <raistlinx@gmail.com>
2020-01-31 22:46:50 +0900

    Add files via upload

---
+++ Chap1Project.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int absolute_value(int x) {
+	int result = 0;
+	if (x < 0) {
+		result = x * -1;
+	}
+	else result = x;
+	return result;
+}
+
+int sum(int x, int y) {
+	int result;
+	result = x + y;
+	return result;
+}
+
+int main() {
+	int my_num = -42;
+	int sum1 = 5;
+	int sum2 = 35;
+	printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+	printf("The sum of %d and %d is %d.\n", sum1, sum2, sum(sum1, sum2));
+}
